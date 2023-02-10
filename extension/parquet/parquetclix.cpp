6548b3ba4a577498e0a1898a550009e326870681
Qing Zheng <qzheng@lanl.gov>
2023-02-10 11:15:39 -0700

    reset footer size to 600 bytes

--- extension/parquet/parquetclix.cpp
+++ extension/parquet/parquetclix.cpp
@@ -84,7 +84,7 @@ struct DeviceFileWrapper : public duckdb::FileHandle {
 	};
 };
 
-template <uint64_t FLAGS_footer_size = 1 << 10, bool FLAGS_disable_cache = false, bool FLAGS_fadv_random = false>
+template <uint64_t FLAGS_footer_size = 600, bool FLAGS_disable_cache = false, bool FLAGS_fadv_random = false>
 class DeviceFileSystem : public duckdb::FileSystem {
 public:
 	std::unique_ptr<duckdb::FileHandle> OpenFile(const std::string &path, uint8_t flags,
